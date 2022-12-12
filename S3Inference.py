489db503bf47eea0ee1f6d6fd1b603dba5034bf4
李铭磊 <44743789+mingleili11@users.noreply.github.com>
2022-12-12 09:09:55 +0800

    Update S3Inference.py

--- S3Inference.py
+++ S3Inference.py
@@ -14,36 +14,19 @@ from tqdm import tqdm
 from torchvision import transforms
 #%%
 batchSize = 32  # batch size
-"""
-trainpath = os.path.join('..', 'Dataset', 'cwt_train_cropimage')
-trainfiles = utils.all_files_under(trainpath)
-testpath = os.path.join('..', 'Dataset', 'cwt_test_cropimage')
-testfiles = utils.all_files_under(testpath)
-data_transform = {
-        "train": transforms.Compose([transforms.Resize([256,256]),transforms.ToTensor()]),
-        "test": transforms.Compose([transforms.Resize([256,256]),transforms.ToTensor()])}
-datasetTrain = MyDataSet(trainfiles, mode='train',transform=data_transform["train"])
-dataloaderTrain = DataLoader(datasetTrain, batch_size=batchSize, shuffle=True)
-datasetTest = MyDataSet(testfiles, mode='test',transform=data_transform["test"])
-dataloaderTest = DataLoader(datasetTest, batch_size=batchSize, shuffle=False)
-"""
+
 #%%
-#trainpath = os.path.join('..', 'Dataset', 'cwt_train_cropimage')
 trainpath = os.path.join('..', 'Dataset', 'cwt_train_set_use_1')
+
 trainfiles = utils.all_files_under(trainpath)
-#testpath = os.path.join('..', 'Dataset', 'cwt_test_cropimage')
 testpath = os.path.join('..', 'Dataset', 'cwt_test_set_use_1')
-#testpath = os.path.join('..', 'Dataset', 'test_set')
+
 testfiles = utils.all_files_under(testpath)
-"""
-data_transform = {
-        "train": transforms.Compose([transforms.Resize([256,256]),transforms.ToTensor()]),
-        "test": transforms.Compose([transforms.Resize([256,256]),transforms.ToTensor()])}
-"""
-#datasetTrain = MyDataSet(trainfiles, mode='train',transform=data_transform["train"])
+
+
 datasetTrain = CWTDataset(trainfiles, mode='train')
-dataloaderTrain = DataLoader(datasetTrain, batch_size=batchSize, shuffle=True)
-#datasetTest = MyDataSet(testfiles, mode='test',transform=data_transform["test"])
+dataloaderTrain = DataLoader(datasetTrain, batch_size=batchSize, shuffle=False)
+
 datasetTest = CWTDataset(testfiles, mode='test')
 dataloaderTest = DataLoader(datasetTest, batch_size=batchSize, shuffle=False)
 #%%
@@ -56,6 +39,7 @@ model.eval()
 filenames, realValues, predValues = [], [], []
 for i, (x, y, names) in enumerate(tqdm(dataloaderTrain)):
     x = x.to(device)
+    # if i==40:
     with torch.no_grad():
         pred,attn = model(x)
         #pred = model(x)
@@ -83,10 +67,10 @@ for i, (x, y, names) in enumerate(tqdm(dataloaderTest)):
         pred,attn = model(x)
         #pred = model(x)
 
-    names = [name[name.index('Bear'):-4] for name in names]
-    filenames.append(names)
-    realValues.append(y.numpy())
-    predValues.append(pred.detach().cpu().numpy())
+        names = [name[name.index('Bear'):-4] for name in names]
+        filenames.append(names)
+        realValues.append(y.numpy())
+        predValues.append(pred.detach().cpu().numpy())
 
 filenames = np.hstack(filenames)
 realValues = np.hstack(realValues)
